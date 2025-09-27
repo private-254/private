dc84ce40dd5a614fc3feba68d04abad0c0b91b86
private-254 <darrellcowgill081@gmail.com>
2025-09-27 18:57:38 +0300

    Update sc.js

--- davecommands/sc.js
+++ davecommands/sc.js
@@ -1,25 +1,28 @@
 const axios = require('axios');
+
 let trashplug = async (m, { trashown, trashcore, reply }) => {
+    if (!m || !m.sender) return; // guard
+
     let regionData = {
         '254': 'Kenya (+254)',
-        '265': 'Malawi (+265)', 
+        '265': 'Malawi (+265)',
         '91': 'India (+91)',
         '90': 'Turkey (+90)',
-        '263': 'Zimbabwe (+263),
+        '263': 'Zimbabwe (+263)',
     };
 
     for (let countryCode in regionData) {
         if (m.sender.startsWith(countryCode)) {
-            global.db.data.users[m.sender].banned = true
+            global.db.data.users[m.sender].banned = true;
             let bannedCountries = Object.values(regionData).join('\n');
             reply(`Sorry, you can't use this bot at this time because your country code has been banned due to spam requests.\n\nBlocked List of Countries:\n${bannedCountries}`);
-            return
+            return;
         }
     }
 };
 
-trashplug.help = ['autoban']
-trashplug.tags = ['security'] 
-trashplug.command = ['autoban']
+trashplug.help = ['autoban'];
+trashplug.tags = ['security'];
+trashplug.command = ['autoban'];
 
 module.exports = trashplug;
