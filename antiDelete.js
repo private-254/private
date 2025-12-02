f7fe67b442b47eff9c21e0852793cc94ce03a668
𝐮𝐧𝐬𝐞𝐞𝐧 <darrellcowgill081@gmail.com>
2025-12-02 18:02:42 +0300

    Update antiDelete.js

--- antiDelete.js
+++ antiDelete.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { downloadContentFromMessage } = require('@whiskeysockets/baileys');
+const { downloadContentFromMessage } = require('@trashcore/baileys');
 
 module.exports = function initAntiDelete(venom, opts = {}) {
 
